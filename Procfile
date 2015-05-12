d41415b8b2d01da77798d00ce0de28c2ba4c3998
Michael Hines <mhines@groupon.com>
2015-05-12 18:50:55 -0500

    Convert startupScript to Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec ruby app.rb
+scraper: bundle exec ruby watir-login.rb
--- startupScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $ALEXA_HOME
-cd server
-bundle exec ruby app.rb &
-cd ..
-cd scraper
-bundle exec ruby watir-login.rb &
